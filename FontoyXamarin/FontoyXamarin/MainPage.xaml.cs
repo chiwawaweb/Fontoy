baec77f4092634eacd16666b358c2175323e50b6
chiwawaweb <git@chiwawaweb.com>
2018-05-22 13:11:42 +0200

    Mise en place de la météo

--- FontoyXamarin/FontoyXamarin/MainPage.xaml.cs
+++ FontoyXamarin/FontoyXamarin/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FontoyXamarin.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -67,7 +68,10 @@ namespace FontoyXamarin
 
         async void OnMeteoClic(object sender, EventArgs e)
         {
-
+            await Navigation.PushAsync(new MeteoPage
+            {
+                BindingContext = new MeteoPage()
+            });
         }
     }
 }
