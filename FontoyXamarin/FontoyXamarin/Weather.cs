09921a39dada6a929f013a98d096075330d5e476
chiwawaweb <git@chiwawaweb.com>
2018-05-23 15:21:37 +0200

    Maj

--- FontoyXamarin/FontoyXamarin/Weather.cs
+++ FontoyXamarin/FontoyXamarin/Weather.cs
@@ -10,12 +10,6 @@ namespace FontoyXamarin
         // ensure that the label appears on all platforms by default.
         public string Title { get; set; } = " ";
         public string Temperature { get; set; } = " ";
-        public string Wind { get; set; } = " ";
-        public string Humidity { get; set; } = " ";
-        public string Visibility { get; set; } = " ";
-        public string Sunrise { get; set; } = " ";
-        public string Sunset { get; set; } = " ";
-
 
         public string TemperatureNow { get; set; } = " ";
         public string PressionNow { get; set; } = " ";
@@ -25,6 +19,22 @@ namespace FontoyXamarin
         public string RisqueNeigeNow { get; set; } = " ";
         public string NebulositeNow { get; set; } = " ";
 
-        
+        public string TemperatureH6 { get; set; } = " ";
+        public string PressionH6 { get; set; } = " ";
+        public string PluieH6 { get; set; } = " ";
+        public string VentVitesseH6 { get; set; } = " ";
+        public string VentDirectionH6 { get; set; } = " ";
+        public string RisqueNeigeH6 { get; set; } = " ";
+        public string NebulositeH6 { get; set; } = " ";
+
+        public string TemperatureH12 { get; set; } = " ";
+        public string PressionH12 { get; set; } = " ";
+        public string PluieH12 { get; set; } = " ";
+        public string VentVitesseH12 { get; set; } = " ";
+        public string VentDirectionH12 { get; set; } = " ";
+        public string RisqueNeigeH12 { get; set; } = " ";
+        public string NebulositeH12 { get; set; } = " ";
+
+
     }
 }
